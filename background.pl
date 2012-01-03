7c51a47cfa1d5600d989d59553bfb55ccec5f381
Torje Hoås Digernes <torjehoa@pvv.org>
2012-01-03 13:46:45 +0100

    more selective on files to put in the slideshow.
    added some regex for file types to the glob.

--- background.pl
+++ background.pl
@@ -4,9 +4,9 @@ use warnings;
 
 my $sleeptimer = 20;
 my $backgrounddirectory = "~/.bg";
-my @images=glob "$backgrounddirectory/*";
 
 while ( 1 ) {
+	my @images=glob "$backgrounddirectory/*{jpg,png,gif}";
 	for my $i ( 0 .. @images-1 ) {
 		system("feh --bg-scale $images[$i]");
 		sleep( $sleeptimer );
